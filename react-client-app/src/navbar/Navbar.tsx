ab51b608ee5d8b1b5ec9069040961fae1bb6b077
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-09-19 17:16:30 -0400

    changes

--- react-client-app/src/navbar/Navbar.tsx
+++ react-client-app/src/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="navbar" >
-          <Link to="/main"><img src="/sosmed.jpg" alt="Logo" className="revature" /></Link>
+          <Link to="/main"><img src="/sosmed.jpg" alt="Logo" className="waiyat" /></Link>
           <div className='navbutton' ><Link to="/profile" style={{ textDecoration: 'none' }}> <h5>Profile</h5></Link></div> 
           <div className='navbutton' ><Link to="/follow" style={{ textDecoration: 'none' }}> <h5>follow</h5></Link></div> 
             
